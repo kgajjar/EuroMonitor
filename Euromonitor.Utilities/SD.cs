69f8860d984e8b314f7a9d907632aa6658a78726
Kieran <rhasquad@gmail.com>
2021-04-17 21:07:45 +0200

    Added Static Details class

---
+++ Euromonitor.Utilities/SD.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euromonitor.Utilities
+{
+    public static class SD
+    {
+        //Add Static Details below this line
+    }
+}
